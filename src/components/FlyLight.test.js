5b253497ade14cb0e3172a495252e4cfd5e5dba3
Hyeongu Hwang <mysticprg@gmail.com>
2017-06-20 06:10:10 +0000

    add FlyLight component

---
+++ src/components/FlyLight.test.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+
+import FlyLight from './FlyLight';
+
+describe('FlyLight', () => {
+   it('should be render without crash', () => {
+       expect(() => {
+           shallow(<FlyLight/>);
+       }).not.toThrow();
+   });
+   
+   it('should be initialize count for lights', () =>{
+       const count = 10;
+       const flyLight = shallow(<FlyLight count={count}/>);
+       
+       const realCount = flyLight.find('.light').length;
+       expect(realCount).toEqual(count);
+   });
+});
