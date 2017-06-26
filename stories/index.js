0094ac3a9b47f6392e95162191e13f4d69a84ea8
mysticPrg <mysticPrg@gmail.com>
2017-06-27 03:52:38 +0900

    #3 cleaning stories

--- stories/index.js
+++ stories/index.js
@@ -4,44 +4,23 @@ import React from 'react';
 
 import { storiesOf } from '@storybook/react';
 import { action } from '@storybook/addon-actions';
-import { linkTo } from '@storybook/addon-links';
-
-import Button from './Button';
-import Welcome from './Welcome';
-
-storiesOf('Welcome', module).add('to Storybook', () => <Welcome showApp={linkTo('Button')} />);
-
-storiesOf('Button', module)
-  .add('with text', () => <Button onClick={action('clicked')}>Hello Button</Button>)
-  .add('with some emoji', () => <Button onClick={action('clicked')}>😀 😎 👍 💯</Button>);
-  
-import Hello from '../src/components/Hello';
-import { INIT_APP } from '../src/actions/ActionTypes';
-storiesOf('MyApp', module).add('Hello', () => <Hello onClick={action(INIT_APP)}/>);
+// import { linkTo } from '@storybook/addon-links';
 
 import Light from '../src/components/light/Light';
 storiesOf('Light', module).add('Basic', () => {
-  const min = 0;
-  const max = 200;
-  let animate;
-  const getAnimateCallback = callback => (animate = callback);
+	const min = 0;
+	const max = 200;
 
-  return (
-    <Light
-      color="#FF0000"
-      alpha="0.5"
-      size="20"
-      x-min={min}
-      x-max={max}
-      y-min={min}
-      y-max={max}
-      getAnimateCallback={getAnimateCallback}
-    />
-  );
-
-  setInterval(() => {
-    if ( animate ) {
-      animate();
-    }
-  }, 500);
+	return (
+		<Light
+			color="#FF0000"
+			alpha="0.5"
+			size="20"
+			x-min={min}
+			x-max={max}
+			y-min={min}
+			y-max={max}
+			onClick={action('click')}
+		/>
+	);
 });
